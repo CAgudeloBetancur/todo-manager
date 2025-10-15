a8751ca3babbe1d3e97532091fa56a1dd747c821
Camilo Agudelo <camiloab97@gmail.com>
2025-10-15 14:15:44 -0500

    Revert "Feature/aggregate root entity value object setup"

--- src/ToDoManager.Composition/Program.cs
+++ src/ToDoManager.Composition/Program.cs
@@ -1,15 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-
-using ToDoManager.Api;
-using ToDoManager.Application;
-using ToDoManager.Infrastructure;
-
-var api = new TodoManagerApi(args, (services, configuration) =>
-{
-	services
-		.AddPresentation()
-		.AddApplication()
-		.AddInfrastructure(configuration);
-});
-
-await api.StartAsync();
+Console.WriteLine("Hello, World!");
