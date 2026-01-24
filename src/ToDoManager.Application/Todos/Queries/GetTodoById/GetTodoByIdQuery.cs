666039090314ebf16cff8da7c52a7b7a604111b4
CAgudeloBetancur <camiloab97@gmail.com>
2026-01-23 21:06:49 -0500

    refactor(GetTodoByIdQuery): implement strongly typed identifier

--- src/ToDoManager.Application/Todos/Queries/GetTodoById/GetTodoByIdQuery.cs
+++ src/ToDoManager.Application/Todos/Queries/GetTodoById/GetTodoByIdQuery.cs
@@ -1,6 +1,7 @@
 ﻿using ErrorOr;
 using MediatR;
+using ToDoManager.Domain.Todos.ValueObjects;
 
 namespace ToDoManager.Application.Todos.Queries.GetTodoById;
 
-public record GetTodoByIdQuery(Guid TodoId) : IRequest< ErrorOr<GetTodoByIdResult>>;
+public record GetTodoByIdQuery(TodoId TodoId) : IRequest< ErrorOr<GetTodoByIdResult>>;
