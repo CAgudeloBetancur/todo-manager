4406a058fe36727d3284b3036f97cf8084fcd7d1
CAgudeloBetancur <camiloab97@gmail.com>
2026-01-26 20:54:35 -0500

    refactor(DeleteTagCommand): implement strongly typed id

--- src/ToDoManager.Application/Tags/Commands/RemoveTag/DeleteTagCommand.cs
+++ src/ToDoManager.Application/Tags/Commands/RemoveTag/DeleteTagCommand.cs
@@ -1,6 +1,7 @@
 ﻿using ErrorOr;
 using MediatR;
+using ToDoManager.Domain.Tags.ValueObjects;
 
 namespace ToDoManager.Application.Tags.Commands.RemoveTag;
 
-public record DeleteTagCommand(Guid Id) : IRequest<ErrorOr<Unit>>;
+public record DeleteTagCommand(TagId TagId) : IRequest<ErrorOr<Unit>>;
