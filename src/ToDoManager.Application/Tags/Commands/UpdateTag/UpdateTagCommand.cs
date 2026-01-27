531879e099dd66d589c990cecd56f94905b24e32
CAgudeloBetancur <camiloab97@gmail.com>
2026-01-26 21:21:24 -0500

    refactor(UpdateTagCommand): implement strongly typed id

--- src/ToDoManager.Application/Tags/Commands/UpdateTag/UpdateTagCommand.cs
+++ src/ToDoManager.Application/Tags/Commands/UpdateTag/UpdateTagCommand.cs
@@ -1,6 +1,7 @@
 ﻿using ErrorOr;
 using MediatR;
+using ToDoManager.Domain.Tags.ValueObjects;
 
 namespace ToDoManager.Application.Tags.Commands.UpdateTag;
 
-public record class UpdateTagCommand(Guid Id, string Name) : IRequest<ErrorOr<Unit>>;
+public record class UpdateTagCommand(TagId TagId, string Name) : IRequest<ErrorOr<Unit>>;
