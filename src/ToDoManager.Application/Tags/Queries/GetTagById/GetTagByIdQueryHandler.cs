9f608b8625c222d1f839b9e07b31e2c9067d9414
CAgudeloBetancur <camiloab97@gmail.com>
2026-01-27 20:01:22 -0500

    refactor(GetTagByIdQueryHandler): enhance code expressiveness and abstraction boundaries

--- src/ToDoManager.Application/Tags/Queries/GetTagById/GetTagByIdQueryHandler.cs
+++ src/ToDoManager.Application/Tags/Queries/GetTagById/GetTagByIdQueryHandler.cs
@@ -3,6 +3,7 @@ using MediatR;
 using ToDoManager.Application.Common.Errors;
 using ToDoManager.Application.Common.Interfaces.Persistence;
 using ToDoManager.Application.Tags.Common;
+using ToDoManager.Domain.Tags;
 using ToDoManager.Domain.Tags.ValueObjects;
 
 namespace ToDoManager.Application.Tags.Queries.GetTagByIdQuery;
@@ -18,10 +19,24 @@ public class GetTagByIdQueryHandler : IRequestHandler<GetTagById.GetTagByIdQuery
 
 	public async Task<ErrorOr<DefaultTagResult>> Handle(GetTagById.GetTagByIdQuery request, CancellationToken cancellationToken)
 	{
-		var tag = await _tagRepository.GetByIdAsync(TagId.Create(request.Id));
-		
-		if(tag is null) return Errors.Tag.NotFound;
+		var tagResult = await GetTagByIdAsync(request.TagId);
 
+		if (tagResult.IsError) return tagResult.Errors;
+
+		var tag = tagResult.Value;
+
+		return MapToResult(tag);
+	}
+
+	private async Task<ErrorOr<Tag>> GetTagByIdAsync(TagId tagId)
+	{
+		var tag = await _tagRepository.GetByIdAsync(tagId);
+		
+		return tag is null ? Errors.Tag.NotFound : tag;
+	}
+
+	private DefaultTagResult MapToResult(Tag tag)
+	{
 		return new DefaultTagResult(tag.Id.Value, tag.Name);
 	}
 }
