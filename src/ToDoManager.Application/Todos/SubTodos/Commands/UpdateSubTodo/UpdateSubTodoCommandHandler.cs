a3d469e889f908067347797ac413e6fb9f653b20
CAgudeloBetancur <camiloab97@gmail.com>
2026-01-29 19:40:56 -0500

    refactor(UpdateSubTodoCommandHandler): improved readability and maintainability by applying clean code principles, refining naming, and encapsulating details

--- src/ToDoManager.Application/Todos/SubTodos/Commands/UpdateSubTodo/UpdateSubTodoCommandHandler.cs
+++ src/ToDoManager.Application/Todos/SubTodos/Commands/UpdateSubTodo/UpdateSubTodoCommandHandler.cs
@@ -4,6 +4,7 @@ using ToDoManager.Application.Common.Errors;
 using ToDoManager.Application.Common.Interfaces.Http;
 using ToDoManager.Application.Common.Interfaces.Persistence;
 using ToDoManager.Application.Common.Interfaces.Persistence.UnitOfWork;
+using ToDoManager.Domain.Todos;
 using ToDoManager.Domain.Todos.ValueObjects;
 using ToDoManager.Domain.Users.ValueObjects;
 
@@ -29,21 +30,46 @@ public class UpdateSubTodoCommandHandler : IRequestHandler<UpdateSubTodoCommand,
 	{
 		var currentUserId = _userAccessor.GetId();
 
-		var todo = await _todoRepository
-			.GetByIdForUserAsync(TodoId.Create(request.TodoId), currentUserId);
+		var todoId = ToTodoId(request.TodoId);
 
-		if (todo is null) return Errors.ToDo.NotFound;
-		
-		todo
-			.UpdateSubTodo(
-				SubTodoId.Create(request.SubTodoId), 
-				request.Title, 
-				request.Description, 
-				request.IsComplete
-				);
-		
-		await _unitOfWork.SaveChangesAsync(cancellationToken);
+		var todoResult = await GetTodoByIdForUser(todoId, currentUserId);
 
-		return Unit.Value;
+		if (todoResult.IsError) return todoResult.Errors;
+		
+		var todo = todoResult.Value;
+		
+		var subTodoId = ToSubTodoId(request.SubTodoId);
+		
+		todo.UpdateSubTodo(
+			subTodoId, 
+			request.Title, 
+			request.Description, 
+			request.IsComplete
+			);
+		
+		var persistenceResult = await _unitOfWork.SaveChangesAsync(cancellationToken);
+
+		return EnsurePersistenceSucceeded(persistenceResult);
+	}
+
+	private ErrorOr<Unit> EnsurePersistenceSucceeded(Error? persistenceResult)
+	{
+		return persistenceResult is not null ? (Error)persistenceResult : Unit.Value;
+	}
+
+	private TodoId ToTodoId(Guid todoIdFromRequest)
+	{
+		return TodoId.Create(todoIdFromRequest);
+	}
+	
+	private SubTodoId ToSubTodoId(Guid subTodoIdFromRequest)
+	{
+		return SubTodoId.Create(subTodoIdFromRequest);
+	}
+
+	private async Task<ErrorOr<Todo>> GetTodoByIdForUser(TodoId todoId, UserId userId)
+	{
+		var todo = await _todoRepository.GetByIdForUserAsync(todoId, userId);
+		return todo is null ? Errors.ToDo.NotFound : todo;
 	}
 }
