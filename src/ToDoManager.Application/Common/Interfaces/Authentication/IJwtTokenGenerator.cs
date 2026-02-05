15f0022d257ed5e33bf3ba5e9fef3a7f31cd8fdc
CAgudeloBetancur <camiloab97@gmail.com>
2026-02-04 20:02:16 -0500

    feature(IJwtTokenGenerator): update GenerateToken method signature to receive a list of roles [string]

--- src/ToDoManager.Application/Common/Interfaces/Authentication/IJwtTokenGenerator.cs
+++ src/ToDoManager.Application/Common/Interfaces/Authentication/IJwtTokenGenerator.cs
@@ -5,5 +5,5 @@ namespace ToDoManager.Application.Common.Interfaces.Authentication;
 
 public interface IJwtTokenGenerator
 {
-	string GenerateToken(User user, IEnumerable<string> role);
+	string GenerateToken(User user, IEnumerable<string> roles);
 }
