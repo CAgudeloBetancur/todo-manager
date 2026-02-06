867be3446b65fad605eaca56fce2f715a57d5da6
CAgudeloBetancur <camiloab97@gmail.com>
2026-02-06 15:36:46 -0500

    feature(IUnitOfWork): Add methods for transaction implementation

--- src/ToDoManager.Application/Common/Interfaces/Persistence/UnitOfWork/IUnitOfWork.cs
+++ src/ToDoManager.Application/Common/Interfaces/Persistence/UnitOfWork/IUnitOfWork.cs
@@ -4,5 +4,8 @@ namespace ToDoManager.Application.Common.Interfaces.Persistence.UnitOfWork;
 
 public interface IUnitOfWork
 {
+	Task BeginTransaction();
+	Task CommitAsync();
+	Task RollbackAsync();
 	Task<Error?> SaveChangesAsync(CancellationToken cancellationToken = default);
 }
