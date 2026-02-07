081091d1e4a20b1a0b0e0b86a1c7e6ef1193ca43
CAgudeloBetancur <camiloab97@gmail.com>
2026-02-06 21:04:45 -0500

    feature(Users): implement IQuery for queries

--- src/ToDoManager.Application/Users/Queries/GetTodosForUser/GetTodosForUserQuery.cs
+++ src/ToDoManager.Application/Users/Queries/GetTodosForUser/GetTodosForUserQuery.cs
@@ -1,6 +1,7 @@
 ﻿using ErrorOr;
 using MediatR;
+using ToDoManager.Application.Common.Interfaces.CQRS;
 
 namespace ToDoManager.Application.Users.Queries.GetTodosForUser;
 
-public record class GetTodosForUserQuery(Guid UserId) : IRequest<ErrorOr<List<GetTodosForUserResult>>>;
+public record class GetTodosForUserQuery(Guid UserId) : IQuery<ErrorOr<List<GetTodosForUserResult>>>;
