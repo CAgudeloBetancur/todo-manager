6614821d715790ff1a202e2f30c51bff40422f88
CAgudeloBetancur <camiloab97@gmail.com>
2026-02-06 20:59:49 -0500

    feature(Tags): implement ICommand and IQuery for commands and queries

--- src/ToDoManager.Application/Tags/Commands/DeleteTag/DeleteTagCommand.cs
+++ src/ToDoManager.Application/Tags/Commands/DeleteTag/DeleteTagCommand.cs
@@ -1,7 +1,8 @@
 ﻿using ErrorOr;
 using MediatR;
+using ToDoManager.Application.Common.Interfaces.CQRS;
 using ToDoManager.Domain.Tags.ValueObjects;
 
 namespace ToDoManager.Application.Tags.Commands.DeleteTag;
 
-public record DeleteTagCommand(Guid TagId) : IRequest<ErrorOr<Unit>>;
+public record DeleteTagCommand(Guid TagId) : ICommand<ErrorOr<Unit>>;
--- src/ToDoManager.Application/Tags/Queries/GetTagById/GetTagByIdQuery.cs
+++ src/ToDoManager.Application/Tags/Queries/GetTagById/GetTagByIdQuery.cs
@@ -1,8 +1,9 @@
 ﻿using ErrorOr;
 using MediatR;
+using ToDoManager.Application.Common.Interfaces.CQRS;
 using ToDoManager.Application.Tags.Common;
 using ToDoManager.Domain.Tags.ValueObjects;
 
 namespace ToDoManager.Application.Tags.Queries.GetTagById;
 
-public record GetTagByIdQuery(Guid TagId) : IRequest<ErrorOr<DefaultTagResult>>;
+public record GetTagByIdQuery(Guid TagId) : IQuery<ErrorOr<DefaultTagResult>>;
