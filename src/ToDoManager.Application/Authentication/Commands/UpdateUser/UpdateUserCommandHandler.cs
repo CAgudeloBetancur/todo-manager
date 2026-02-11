040da243657c2bb9b34f43aec1cfea961672cea5
CAgudeloBetancur <camiloab97@gmail.com>
2026-02-11 10:21:14 -0500

    refactor(UpdateUserCommandHandler): enhance code expressiveness and encapsulation

--- src/ToDoManager.Application/Authentication/Commands/UpdateUser/UpdateUserCommandHandler.cs
+++ src/ToDoManager.Application/Authentication/Commands/UpdateUser/UpdateUserCommandHandler.cs
@@ -2,9 +2,11 @@
 using MediatR;
 using ToDoManager.Application.Authentication.Common;
 using ToDoManager.Application.Authentication.Common.Persistence;
+using ToDoManager.Application.Common.Behaviors;
 using ToDoManager.Application.Common.Errors;
 using ToDoManager.Application.Common.Interfaces.Authentication;
 using ToDoManager.Domain.Users;
+using ToDoManager.Domain.Users.ValueObjects;
 
 namespace ToDoManager.Application.Authentication.Commands.UpdateUser;
 
@@ -22,20 +24,25 @@ public class UpdateUserCommandHandler : IRequestHandler<UpdateUserCommand, Error
 		CancellationToken cancellationToken
 		)
 	{
-		var domainUser = CreateUserFromRequest(
-			request.UserId, 
-			request.DisplayName, 
-			request.Email, 
-			request.FirstName, 
-			request.LastName
-			);
+		var userId = UserId.Create(request.UserId);
 		
-		var updateUserResult = await _userRepository.UpdateUserAsync(domainUser);
+		var userResult = await _userRepository.FindByIdAsync(userId);
+
+		var user = EnsureUserExists(userResult);
+
+		if (user.IsError) return user.Errors;
+		
+		var updateUserResult = await _userRepository.UpdateUserAsync(user.Value);
 		
 		if(!updateUserResult.Succeeded) 
 			return MapToValidationErrors(updateUserResult.Errors);
 
 		return Unit.Value;
+	}
+
+	private ErrorOr<User> EnsureUserExists(User? userResult)
+	{
+		return userResult is null ? Errors.User.NotFound : userResult;
 	}
 
 	private User CreateUserFromRequest(
