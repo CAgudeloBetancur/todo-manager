4091248df1797b7f3513bea8669584104cebacdd
CAgudeloBetancur <camiloab97@gmail.com>
2026-02-10 21:13:46 -0500

    feature(ChangeEmailCommandHandler): implement logic for change email using user repository

--- src/ToDoManager.Application/Authentication/Commands/ChangeEmail/ChangeEmailCommandHandler.cs
+++ src/ToDoManager.Application/Authentication/Commands/ChangeEmail/ChangeEmailCommandHandler.cs
@@ -1,12 +1,41 @@
 ﻿using ErrorOr;
 using MediatR;
+using ToDoManager.Application.Authentication.Common.Persistence;
+using ToDoManager.Application.Common.Interfaces.Authentication;
+using ToDoManager.Domain.Users.ValueObjects;
 
 namespace ToDoManager.Application.Authentication.Commands.ChangeEmail;
 
 public class ChangeEmailCommandHandler : IRequestHandler<ChangeEmailCommand, ErrorOr<Unit>>
 {
-	public Task<ErrorOr<Unit>> Handle(ChangeEmailCommand request, CancellationToken cancellationToken)
+	private readonly IUserRepository _userRepository;
+
+	public ChangeEmailCommandHandler(IUserRepository userRepository)
 	{
-		throw new NotImplementedException();
+		_userRepository = userRepository;
+	}
+
+	public async Task<ErrorOr<Unit>> Handle(ChangeEmailCommand request, CancellationToken cancellationToken)
+	{
+		var userId = ToUserId(request.UserId);
+		
+		var changeEmailResult = await _userRepository.ChangeEmailAsync(userId, request.NewEmail);
+		
+		if(!changeEmailResult.Succeeded)
+			return MapToValidationErrors(changeEmailResult.Errors);
+
+		return Unit.Value;
+	}
+
+	private static UserId ToUserId(Guid requestUserId)
+	{
+		return UserId.Create(requestUserId);
+	}
+	
+	private static List<Error> MapToValidationErrors(IEnumerable<AuthenticationError> errors)
+	{
+		return errors
+			.Select(e => Error.Validation(e.Code, e.Description))
+			.ToList();
 	}
 }
