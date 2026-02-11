e8bac3e623fd98af40355f07ac85af23e4e85561
CAgudeloBetancur <camiloab97@gmail.com>
2026-02-10 21:29:55 -0500

    feature(ChangePasswordCommand): implement ChangePasswordCommand, its handler and validator

---
+++ src/ToDoManager.Application/Authentication/Commands/ChangePassword/ChangePasswordCommandHandler.cs
@@ -0,0 +1,39 @@
+﻿using ErrorOr;
+using MediatR;
+using ToDoManager.Application.Authentication.Common.Persistence;
+using ToDoManager.Application.Common.Interfaces.Authentication;
+using ToDoManager.Application.Common.Interfaces.Http;
+
+namespace ToDoManager.Application.Authentication.Commands.ChangePassword;
+
+public class ChangePasswordCommandHandler : IRequestHandler<ChangePasswordCommand, ErrorOr<Unit>>
+{
+	private readonly IUserAccessor _userAccessor;
+	private readonly IUserRepository _userRepository;
+
+	public ChangePasswordCommandHandler(IUserAccessor userAccessor, IUserRepository userRepository)
+	{
+		_userAccessor = userAccessor;
+		_userRepository = userRepository;
+	}
+
+	public async Task<ErrorOr<Unit>> Handle(ChangePasswordCommand request, CancellationToken cancellationToken)
+	{
+		var userId = _userAccessor.GetId();
+		
+		var changePasswordResult = await _userRepository
+			.ChangePasswordAsync(userId, request.CurrentPassword, request.NewPassword);
+
+		if (!changePasswordResult.Succeeded)
+			return MapToValidationErrors(changePasswordResult.Errors);
+		
+		return Unit.Value;
+	}
+	
+	private static List<Error> MapToValidationErrors(IEnumerable<AuthenticationError> errors)
+	{
+		return errors
+			.Select(e => Error.Validation(e.Code, e.Description))
+			.ToList();
+	}
+}
