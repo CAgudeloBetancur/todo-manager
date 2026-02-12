4b5dfa4b757639c03e5bc77bfbb9ad5aaeb829c0
CAgudeloBetancur <camiloab97@gmail.com>
2026-02-11 21:19:46 -0500

    feature(IRefreshTokenRespository): implement refresh token repository interface and refresh token dto

---
+++ src/ToDoManager.Application/Common/Interfaces/Persistence/IRefreshTokenRepository.cs
@@ -0,0 +1,11 @@
+﻿using ToDoManager.Application.Common.Interfaces.Persistence.DTOs;
+
+namespace ToDoManager.Application.Common.Interfaces.Persistence;
+
+public interface IRefreshTokenRepository
+{
+	Task<RefreshTokenDto?> GetByTokenAsync(string token);
+	Task AddAsync(RefreshTokenDto refreshToken);
+	Task InvalidateAsync(string token);
+	Task <IEnumerable<RefreshTokenDto>> GetByUserIdAsync(Guid userId);
+}
