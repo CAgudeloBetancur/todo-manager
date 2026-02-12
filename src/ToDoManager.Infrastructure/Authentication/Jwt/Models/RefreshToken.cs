adf4621da36c08819ffe448ace7a1d2a491c584f
CAgudeloBetancur <camiloab97@gmail.com>
2026-02-11 20:41:07 -0500

    feature(RefreshToken): create db model for refresh token

---
+++ src/ToDoManager.Infrastructure/Authentication/Jwt/Models/RefreshToken.cs
@@ -0,0 +1,10 @@
+﻿namespace ToDoManager.Infrastructure.Authentication.Jwt.Models;
+
+public class RefreshToken
+{
+	public Guid Id { get; set; }
+	public string Token { get; set; }
+	public DateTime ExpiresAt { get; set; }
+	public Guid UserId { get; set; }
+	public bool IsRevoked { get; set; }
+}
