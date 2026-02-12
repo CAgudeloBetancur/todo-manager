e2328054a2fd85c58c66dd6dbec856ba1228400a
CAgudeloBetancur <camiloab97@gmail.com>
2026-02-11 21:07:29 -0500

    feature(Migratios): create refresh token setup migration

--- src/ToDoManager.Infrastructure/Migrations/20260212015017_RefreshTokenSetup.Designer.cs
+++ src/ToDoManager.Infrastructure/Migrations/20260212015017_RefreshTokenSetup.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using ToDoManager.Infrastructure.Data.Persistence;
@@ -11,9 +12,11 @@ using ToDoManager.Infrastructure.Data.Persistence;
 namespace ToDoManager.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260212015017_RefreshTokenSetup")]
+    partial class RefreshTokenSetup
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
