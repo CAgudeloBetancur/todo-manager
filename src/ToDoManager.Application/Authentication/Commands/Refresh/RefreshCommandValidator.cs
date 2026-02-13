8bec6e24aa48df9db0c4e05dcab559c1d337444c
CAgudeloBetancur <camiloab97@gmail.com>
2026-02-12 20:18:05 -0500

    feature(RefreshCommandValidator): implement validator for refresh command

---
+++ src/ToDoManager.Application/Authentication/Commands/Refresh/RefreshCommandValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace ToDoManager.Application.Authentication.Commands.Refresh;
+
+public class RefreshCommandValidator : AbstractValidator<RefreshCommand>
+{
+	public RefreshCommandValidator()
+	{
+		RuleFor(x => x.RefreshToken)
+			.NotEmpty()
+			.MaximumLength(200)
+			.WithMessage("Refresh token is required");
+	}
+}
