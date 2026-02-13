c4c45c5018e5344e0858592aefd12d7d38ea0f28
CAgudeloBetancur <camiloab97@gmail.com>
2026-02-12 20:16:25 -0500

    feature(RefreshCommand): create command for refresh token flow

---
+++ src/ToDoManager.Application/Authentication/Commands/Refresh/RefreshCommand.cs
@@ -0,0 +1,7 @@
+﻿using ErrorOr;
+using MediatR;
+using ToDoManager.Application.Authentication.Common;
+
+namespace ToDoManager.Application.Authentication.Commands.Refresh;
+
+public record RefreshCommand(string RefreshToken) : IRequest<ErrorOr<AuthenticationResult>>;
