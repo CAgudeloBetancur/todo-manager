37ed4695f0f1fcbbe4d0d2f86f916a767895722e
CAgudeloBetancur <camiloab97@gmail.com>
2026-02-13 21:31:19 -0500

    feature(LogoutCommand): add IAllowAnonymous

--- src/ToDoManager.Application/Authentication/Commands/Logout/LogoutCommand.cs
+++ src/ToDoManager.Application/Authentication/Commands/Logout/LogoutCommand.cs
@@ -1,6 +1,7 @@
 ﻿using ErrorOr;
 using MediatR;
+using ToDoManager.Application.Authentication.Common.Interfaces;
 
 namespace ToDoManager.Application.Authentication.Commands.Logout;
 
-public record LogoutCommand(string RefreshToken) : IRequest<ErrorOr<Unit>>;
+public record LogoutCommand(string RefreshToken) : IRequest<ErrorOr<Unit>>, IAllowAnonymous;
