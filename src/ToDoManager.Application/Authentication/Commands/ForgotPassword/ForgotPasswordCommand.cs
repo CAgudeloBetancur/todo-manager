4e2f02018c00487457330cc7620957e446d9ae02
CAgudeloBetancur <camiloab97@gmail.com>
2026-02-16 21:20:13 -0500

    fix(ForgotPasswordCommand/ResetPasswordCommand): add interface IAllowAnonymous to avoid auth

--- src/ToDoManager.Application/Authentication/Commands/ForgotPassword/ForgotPasswordCommand.cs
+++ src/ToDoManager.Application/Authentication/Commands/ForgotPassword/ForgotPasswordCommand.cs
@@ -1,7 +1,8 @@
 ﻿using ErrorOr;
 using MediatR;
+using ToDoManager.Application.Authentication.Common.Interfaces;
 using ToDoManager.Application.Common.Interfaces.CQRS;
 
 namespace ToDoManager.Application.Authentication.Commands.ForgotPassword;
 
-public record ForgotPasswordCommand(string Email) : ICommand<ErrorOr<Unit>>;
+public record ForgotPasswordCommand(string Email) : ICommand<ErrorOr<Unit>>, IAllowAnonymous;
--- src/ToDoManager.Application/Authentication/Commands/ResetPassword/ResetPasswordCommand.cs
+++ src/ToDoManager.Application/Authentication/Commands/ResetPassword/ResetPasswordCommand.cs
@@ -1,5 +1,6 @@
 ﻿using ErrorOr;
 using ToDoManager.Application.Authentication.Common;
+using ToDoManager.Application.Authentication.Common.Interfaces;
 using ToDoManager.Application.Common.Interfaces.CQRS;
 
 namespace ToDoManager.Application.Authentication.Commands.ResetPassword;
@@ -8,4 +9,4 @@ public record ResetPasswordCommand(
     string Email,
     string ResetToken,
     string NewPassword
-    ) : ICommand<ErrorOr<AuthenticationResult>>;
+    ) : ICommand<ErrorOr<AuthenticationResult>>, IAllowAnonymous;
