877540736e650c70e79db8b2a2765e8311821a82
CAgudeloBetancur <camiloab97@gmail.com>
2026-02-16 20:25:24 -0500

    feature(ForgotPasswordCommand): implement forgot password command, validator and handler

---
+++ src/ToDoManager.Application/Authentication/Commands/ForgotPassword/ForgotPasswordCommand.cs
@@ -0,0 +1,6 @@
+﻿using ErrorOr;
+using MediatR;
+
+namespace ToDoManager.Application.Authentication.Commands.ForgotPassword;
+
+public record ForgotPasswordCommand(string Email) : IRequest<ErrorOr<Unit>>;
---
+++ src/ToDoManager.Application/Authentication/Commands/ForgotPassword/ForgotPasswordCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using ErrorOr;
+using MediatR;
+using ToDoManager.Application.Common.Interfaces.Authentication;
+using ToDoManager.Application.Common.Interfaces.Services;
+
+namespace ToDoManager.Application.Authentication.Commands.ForgotPassword;
+
+public class ForgotPasswordCommandHandler : IRequestHandler<ForgotPasswordCommand, ErrorOr<Unit>>
+{
+    private readonly IUserRepository _userRepository;
+    private readonly IEmailService _emailService;
+
+    public ForgotPasswordCommandHandler(IUserRepository userRepository, IEmailService emailService)
+    {
+        _userRepository = userRepository;
+        _emailService = emailService;
+    }
+
+    public async Task<ErrorOr<Unit>> Handle(ForgotPasswordCommand request, CancellationToken cancellationToken)
+    {
+        var user = await _userRepository.FindByEmailAsync(request.Email);
+        
+        if (user is null)
+            return Unit.Value;
+
+        var resetToken = await _userRepository.GeneratePasswordResetTokenAsync(user);
+
+        await _emailService.SendPasswordResetEmailAsync(user.Email.Value, resetToken, cancellationToken);
+
+        return Unit.Value;
+    }
+}
